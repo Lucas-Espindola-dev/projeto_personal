41ef2150aebdd8b883f86cd521ec4920bb5cb0d0
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-09-11 16:12:37 -0300

    Creating the Stretchings Model

--- workouts/models.py
+++ workouts/models.py
@@ -11,9 +11,17 @@ class Exercises(models.Model):
 
 
 class Cardio(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=255)
     time = models.IntegerField(blank=True, null=True)
     kcal = models.IntegerField(blank=True, null=True)
+    instructional_video = models.CharField(max_length=255)
+    comments = models.TextField(blank=True, null=True)
+    workout = models.ForeignKey
+
+
+class Stretchings(models.Model):
+    name = models.CharField(max_length=255)
+    time = models.TimeField()
     instructional_video = models.CharField(max_length=255)
     comments = models.TextField(blank=True, null=True)
     workout = models.ForeignKey
