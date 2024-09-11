31cfb855cc9061683cfed693ac38ec73ccb3143b
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-09-11 16:08:37 -0300

    Creating the Cardio Model

--- workouts/models.py
+++ workouts/models.py
@@ -6,7 +6,15 @@ class Exercises(models.Model):
     sets = models.IntegerField()
     repetitions = models.IntegerField()
     instructional_video = models.CharField(max_length=255)
-    comments = models.TextField()
-    workout = models.ManyToOneRel()
+    comments = models.TextField(blank=True, null=True)
+    workout = models.ForeignKey
 
+
+class Cardio(models.Model):
+    name = models.CharField()
+    time = models.IntegerField(blank=True, null=True)
+    kcal = models.IntegerField(blank=True, null=True)
+    instructional_video = models.CharField(max_length=255)
+    comments = models.TextField(blank=True, null=True)
+    workout = models.ForeignKey
 
