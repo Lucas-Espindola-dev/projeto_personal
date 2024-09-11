fe505ddd0c6e83386791e22d496117e570ffbdcc
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-09-11 15:57:59 -0300

    Creating the exercises model

--- workouts/models.py
+++ workouts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Exercises(models.Model):
+    name = models.CharField(max_length=255)
+    sets = models.IntegerField()
+    repetitions = models.IntegerField()
+    instructional_video = models.CharField(max_length=255)
+    comments = models.TextField()
+    workout = models.ManyToOneRel()
+
+
